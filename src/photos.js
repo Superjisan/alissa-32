54d220d75ee788a0223b591d1ce2beeea166c0f2
Jisan Zaman <jisan.zaman@dynamo.works>
2019-08-13 19:39:04 -0400

    updated favicon. Added all the captions

--- src/photos.js
+++ src/photos.js
@@ -55,180 +55,180 @@ export const photos = [
         src: cherryBlossoms,
         width: 6,
         height: 4,
-        title: "",
+        title: "Thank you for going with me to see the cherry blossoms even under heavy duress from tourists. You always go out of your way to make what I want happen. I love you for that",
     },
     {
         src: bengaliWedding,
         width: 4,
         height: 4,
-        title: ""
+        title: "Thank you for being my +1 at Roshni's wedding. You rocked that sari and hopefully now you realize where my selfie taking ways come from."
     },
     {
         src: cebTower,
         width: 3,
         height: 4,
-        title: ""
+        title: "Thank you for going to see the top of the DMV area with me. Hopefully you are happy with the free admission there now that I made you move. I remember our first date there as one of the best times in our early courtship."
     },
     {
         src: canvassing,
         width: 3,
         height: 4,
-        title: ""
+        title: "Thank you for wanting to get involved in local politics with me. This was truly a memorable canvassing as we all took our safest race. Hope you're ready for more in the upcoming months."
     },
     {
         src: kickballCostume,
         width: 4,
         height: 4,
-        title: ""
+        title: "Thank you for going overboard and then some for this activity. I love how much time and effort we put into this silly adventure. But I love your passion for kickball and competition even more."
     },
     {
         src: fancyMatchingPink,
         width: 4,
         height: 4,
-        title: ""
+        title: "Thank you for being the most utterly overdressed at the wedding reception for my friend Elisa. We totally rocked this costume babe."
     },
     {
         src: july4bbq,
         width: 3,
         height: 4,
-        title: ""
+        title: "Thank you for welcoming me to your family and showing me a glimpse of where you grow up and how you guys celebrate America. It was loads of fun."
     },
     {
         src: katieMinklesPhotobooth,
         width: 3,
         height: 3,
-        title: ""
+        title: "Thank you for adopting my silly 'JISAN FACES' and being a wonderful partner during that stressful weekend."
     },
     {
         src: dynamoHolidayParty,
         width: 3,
         height: 3,
-        title: ""
+        title: "Thank you for being my good luck charm at that party. I've never won a raffle before that and it was clearly you. We also look so adorable here."
     },
     {
         src: itmfa,
         width: 3,
         height: 3,
-        title: ""
+        title: "Thank you for sharing my passionate hatred of the residence of Cheeto Mussolini. Let's work in the future to ensure people like him never get to be POTUS again."
     },
     {
         src: cwc2019,
         width: 3,
         height: 3,
-        title: ""
+        title: "Thank you for going to see an 8 hour Cricket game with me. And thank you for wathing the 3 hour movie Lagaan in Scotland in prep. I owe you one haha."
     },
     {
         src: washingtonFabulous,
         width: 3,
         height: 4,
-        title: ""
+        title: "Thank you for always being your fabulous self. I am fortunate to have found you in this grand city"
     },
     {
         src: axeThrowing,
         width: 5,
         height: 3,
-        title: ""
+        title: "Thank you for coming with me to another work activity. Hopefully you now realize me with an axe is a very dangerous combo"
     },
     {
         src: withKatieMinklesVisit,
         width: 4,
         height: 3,
-        title: ""
+        title: "Thank you for being so warm and hospitable to my friends. They loved you and you did a great job in not telling them what to do as we played Harry Potter. "
     },
     {
         src: trivia,
         width: 4,
         height: 3,
-        title: ""
+        title: "Thank you for introducing me to your longtime friends at trivia. I am sorry I don't go anymore but I did enjoy meeting them and I promise to go again soon."
     },
     {
         src: lavaKuki,
         width: 4,
         height: 3,
-        title: ""
+        title: "Thank you for sharing your musical talents with me. The fact that you wanted to sing the song without asking meant a lot to me. We did such an amazing job of it too love. We should do more of that"
     },
     {
         src: localElections,
         width: 3,
         height: 3,
-        title: ""
+        title: "Thank you for going to the firehouse primary with me. These things are boring but they are amazingly important in keeping our democracy healthy. Thanks for sharing in my passion love."
     },
     {
         src: nycTrip,
         width: 4,
         height: 3,
-        title: ""
+        title: "Thanks for coming with me to NYC and meeting all my friends. I am sorry that it didn't work out with the World Trade Center. But I hope the rest of the day was more fun babe"
     },
     {
         src: londonAdv,
         width: 4,
         height: 3,
-        title: ""
+        title: "Thank you for engaging in all of the fun adventures in London with me. Sorry I was a grumpy and pained man. But I loved your colleagues and it was great to see how they do things on the other side of the pond."
     },
     {
         src: skydiving,
         width: 4,
         height: 3,
-        title: ""
+        title: "Thanks for taking me skydiving babe. That was the most next level thing I've done. Probably don't want to do it again anytime soon. But I'll accompany you the next time you feel up to it."
     },
     {
         src: withAngela,
         width: 5,
         height: 3,
-        title: ""
+        title: "Thanks for introducing me to Angela. I hope she liked me. It was a lot of fun to hang out with her, even with her dislike of Endgame."
     },
     {
         src: hamilton,
         width: 3,
         height: 3,
-        title: ""
+        title: "Thanks for going to Hamilton with me. That was amazing! And I enjoyed the extracurricular activities afterwards as well."
     },
     {
         src: renaissanceFest,
         width: 3,
         height: 3,
-        title: ""
+        title: "Thanks for taking me to my first Ren Faire. I loved it. Next time, I can't wait to get all the Turkey legs."
     },
     {
         src: shenandoahTrip,
         width: 4,
         height: 3,
-        title: ""
+        title: "Thanks for organizing and planning the Shenandoah trip. It was great to get away and enjoy the outdoors and indoors with just us."
     },
     {
         src: withPei,
         width: 4,
         height: 3,
-        title: ""
+        title: "Thanks for being so nice to Pei. He is your #1 fan and is incredibly happy for me that I found such an amazing woman to share my life with."
     },
     {
         src: sanDiego,
         width: 3,
         height: 4,
-        title: ""
+        title: "Thanks for inviting me and letting me stay in San Diego hotel. It was a much needed break I needed before the insane-ness of the bootcamp class"
     },
     {
         src: oneYearAnniversary,
         width: 3,
         height: 3,
-        title: ""
+        title: "Thanks for baring with me as I fucked up our 1 year anniversary celebration. At least we had a story to tell. I promise I won't lose the ball if we make it to 2 years."
     },
     {
         src: kayaking,
         width: 4,
         height: 3,
-        title: ""
+        title: "Thanks for going kayaking on that kayak boat 😜. I love you and I am glad you were there to not have the boat capsize."
     },
     {
         src: houseWarmingFeb2019,
         width: 4,
         height: 3,
-        title: ""
+        title: "Thanks for inviting all of your friends and fam to the housewarming party. Can't wait to run it again in a month or so."
     },
     {
         src: famVisitLondon,
         width: 3,
         height: 3,
-        title: ""
+        title: "Thanks for taking the time to visit my cousins in London. And look, you are learning the art of the photobomb. I am so proud."
     }
 ];
