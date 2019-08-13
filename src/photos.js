bce02eeb6d9464244c969723092d1b867023b850
Jisan Zaman <jisan.zaman@dynamo.works>
2019-08-13 19:06:53 -0400

    fixed width proportions

--- src/photos.js
+++ src/photos.js
@@ -1,18 +1,36 @@
+import * as axeThrowing from  "./images/axe_throwing.png";             
+import * as lavaKuki from  "./images/lavakuki_2018.jpg";
+import * as bengaliWedding from  "./images/bengali_wedding.jpg";          
+import * as localElections from  "./images/local_elections_2018.jpg";
+import * as canvassing from  "./images/canvassing.jpg";               
+import * as londonAdv from  "./images/london_adventures.jpg";
+import * as cebTower from  "./images/ceb_tower.jpg";                
+import * as nycTrip from  "./images/nyc_2018_with_friends.jpg";
+import * as cherryBlossoms from  "./images/cherry_blossoms_2018.jpg";     
+import * as oneYearAnniversary from  "./images/one_year_cricket.jpg";
+import * as cwc2019 from  "./images/cwc_2019.jpg";                 
+import * as renaissanceFest from  "./images/renaissance_fest_2018.jpg";
+import * as washingtonFabulous from  "./images/fabulous_pose_dc.jpg";         
+import * as kayaking from  "./images/row_boating.jpg";
+import * as kickballCostume from  "./images/fall_kickball_ball.jpg";       
+import * as sanDiego from  "./images/sandiego_2018.jpg";
+import * as fancyMatchingPink from  "./images/fancy_pink.jpg";               
+import * as shenandoahTrip from  "./images/shenandoah_2018.jpg";
+import * as hamilton from  "./images/hamilton.jpg";                 
+import * as skydiving from  "./images/skydiving.jpg";
+import * as trivia from  "./images/trivia.png";
+import * as dynamoHolidayParty from  "./images/holiday_work_party2018.jpg";   
+import * as houseWarmingFeb2019 from  "./images/housewarming_feb_2018.jpg";    
+import * as withAngela from  "./images/with_angela.jpg";
+import * as itmfa from  "./images/itmfa_white_house.jpg";        
+import * as famVisitLondon from  "./images/with_fam_london_2018.jpg";
+import * as katieMinklesPhotobooth from  "./images/jisan_face_photobooth.jpg";    
+import * as withPei from  "./images/with_pei.jpg";
+import * as july4bbq from  "./images/july4_2019_bbq.jpg";           
+import * as withKatieMinklesVisit from  "./images/katie_minkles_visit.jpg";      
 import * as theaNap from "./images/with_thea_nap.jpg";
-// import * as theaNap from "./images/with_thea_nap.jpg";
-// import * as theaNap from "./images/with_thea_nap.jpg";
-// import * as theaNap from "./images/with_thea_nap.jpg";
-// import * as theaNap from "./images/with_thea_nap.jpg";
-// import * as theaNap from "./images/with_thea_nap.jpg";
-// import * as theaNap from "./images/with_thea_nap.jpg";
-// import * as theaNap from "./images/with_thea_nap.jpg";
-// import * as theaNap from "./images/with_thea_nap.jpg"
-// import * as theaNap from "./images/with_thea_nap.jpg"
-// import * as theaNap from "./images/with_thea_nap.jpg"
-// import * as theaNap from "./images/with_thea_nap.jpg"
-// import * as theaNap from "./images/with_thea_nap.jpg"
-// import * as theaNap from "./images/with_thea_nap.jpg"
-
+import * as veganDinner from "./images/vegan_fancy_dinner_2018.jpg";
+import * as happyHour from "./images/happy_hour_bootcamp_2018.jpg";
 
 export const photos = [
     {
@@ -22,152 +40,195 @@ export const photos = [
         title: "Thank you for introducing me to your family. Especially this little cutie of a rascal. They are amazing and I am incredibly privileged to know them."
     },
     {
-        src: theaNap,
+        src: veganDinner,
         width: 3,
         height: 4,
         title: "Thank you for going vegan with me for a whole month. And keeping me honest. Elizabeth's Gone Raw was amazing. It was even more amazing to spend it there with you."
     },
     {
-        src: theaNap,
+        src: happyHour,
         width: 8,
         height: 4,
         title: "Thank you for putting up with my second job. It takes a special person to deal with a partner with no weekends and no nights free. It was great to have you at this Happy Hour over at Crafthouse. You are an amazingly patient and kind partner"
     },
     {
-        src: theaNap,
-        width: 5,
+        src: cherryBlossoms,
+        width: 6,
+        height: 4,
+        title: "",
+    },
+    {
+        src: bengaliWedding,
+        width: 4,
         height: 4,
         title: ""
     },
     {
-        src: theaNap,
-        width: 4,
-        height: 4
-    },
-    {
-        src: theaNap,
+        src: cebTower,
         width: 3,
-        height: 4
+        height: 4,
+        title: ""
     },
     {
-        src: theaNap,
-        width: 4,
-        height: 4
-    },
-    {
-        src: theaNap,
-        width: 4,
-        height: 4
-    },
-    {
-        src: theaNap,
+        src: canvassing,
         width: 3,
-        height: 4
+        height: 4,
+        title: ""
     },
     {
-        src: theaNap,
-        width: 5,
-        height: 3
+        src: kickballCostume,
+        width: 4,
+        height: 4,
+        title: ""
     },
     {
-        src: theaNap,
-        width: 5,
-        height: 3
+        src: fancyMatchingPink,
+        width: 4,
+        height: 4,
+        title: ""
     },
     {
-        src: theaNap,
-        width: 5,
-        height: 3
+        src: july4bbq,
+        width: 3,
+        height: 4,
+        title: ""
     },
     {
-        src: theaNap,
-        width: 5,
-        height: 3
+        src: katieMinklesPhotobooth,
+        width: 3,
+        height: 3,
+        title: ""
     },
     {
-        src: theaNap,
-        width: 5,
-        height: 3
+        src: dynamoHolidayParty,
+        width: 3,
+        height: 3,
+        title: ""
     },
     {
-        src: theaNap,
-        width: 5,
-        height: 3
+        src: itmfa,
+        width: 3,
+        height: 3,
+        title: ""
     },
     {
-        src: theaNap,
-        width: 5,
-        height: 3
+        src: cwc2019,
+        width: 3,
+        height: 3,
+        title: ""
     },
     {
-        src: theaNap,
-        width: 5,
-        height: 3
+        src: washingtonFabulous,
+        width: 3,
+        height: 4,
+        title: ""
     },
     {
-        src: theaNap,
+        src: axeThrowing,
         width: 5,
-        height: 3
+        height: 3,
+        title: ""
     },
     {
-        src: theaNap,
-        width: 5,
-        height: 3
+        src: withKatieMinklesVisit,
+        width: 4,
+        height: 3,
+        title: ""
     },
     {
-        src: theaNap,
-        width: 5,
-        height: 3
+        src: trivia,
+        width: 4,
+        height: 3,
+        title: ""
     },
     {
-        src: theaNap,
-        width: 5,
-        height: 3
+        src: lavaKuki,
+        width: 4,
+        height: 3,
+        title: ""
     },
     {
-        src: theaNap,
-        width: 5,
-        height: 3
+        src: localElections,
+        width: 3,
+        height: 3,
+        title: ""
     },
     {
-        src: theaNap,
-        width: 5,
-        height: 3
+        src: nycTrip,
+        width: 4,
+        height: 3,
+        title: ""
     },
     {
-        src: theaNap,
-        width: 5,
-        height: 3
+        src: londonAdv,
+        width: 4,
+        height: 3,
+        title: ""
     },
     {
-        src: theaNap,
-        width: 5,
-        height: 3
+        src: skydiving,
+        width: 4,
+        height: 3,
+        title: ""
     },
     {
-        src: theaNap,
+        src: withAngela,
         width: 5,
-        height: 3
+        height: 3,
+        title: ""
     },
     {
-        src: theaNap,
-        width: 5,
-        height: 3
+        src: hamilton,
+        width: 3,
+        height: 3,
+        title: ""
     },
     {
-        src: theaNap,
-        width: 5,
-        height: 3
+        src: renaissanceFest,
+        width: 3,
+        height: 3,
+        title: ""
     },
     {
-        src: theaNap,
-        width: 5,
-        height: 3
+        src: shenandoahTrip,
+        width: 4,
+        height: 3,
+        title: ""
     },
     {
-        src: theaNap,
-        width: 5,
-        height: 3
+        src: withPei,
+        width: 4,
+        height: 3,
+        title: ""
+    },
+    {
+        src: sanDiego,
+        width: 3,
+        height: 4,
+        title: ""
+    },
+    {
+        src: oneYearAnniversary,
+        width: 3,
+        height: 3,
+        title: ""
+    },
+    {
+        src: kayaking,
+        width: 4,
+        height: 3,
+        title: ""
+    },
+    {
+        src: houseWarmingFeb2019,
+        width: 4,
+        height: 3,
+        title: ""
+    },
+    {
+        src: famVisitLondon,
+        width: 3,
+        height: 3,
+        title: ""
     }
-
 ];
