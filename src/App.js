6a10b699b44fbb20f4a267503f7015fd67c09ea5
Jisan Zaman <jisan.zaman@dynamo.works>
2019-08-13 17:20:13 -0400

    added local images

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   const [viewerIsOpen, setViewerIsOpen] = useState(false);
 
   const openLightbox = useCallback((event, { photo, index }) => {
+    event.stopPropagation();
     setCurrentImage(index);
     setViewerIsOpen(true);
   }, []);
@@ -23,10 +24,7 @@ function App() {
       <h1>Happy Birthday Alissa Ortman</h1>
       <p>I am so thankful you are in my life bae. I made you a website of 32 images and how thankful I am to share all those memories with you. Love,
       Jisan</p>
-      <a 
-        href='https://photos.google.com/share/AF1QipNzEj98fcCRdo5XTrpe2rEZwX3Bz6FXpi8QCMGIsnOogZBGxZ1ARhYeaa5SH0q1Sg?key=TFNjS1pXQmFieXd4NDhwdTNPY3ZlT3oySVpaNV9R&source=ctrlq.org'>
-          {/* <img src='https://lh3.googleusercontent.com/_vmWevyFAySSaEnKhOIy04u6b1jPoBn3O2rEAamipu8sqddYcZPd186pjDCfYjPeQyxqWciT6TFNt33wjpbKDLk25k5_6KRG-mZJo0vr7_34jyZZ-kmBZNtRensg-CDd2ibGur3uQA=w2400' /> */}
-      </a>
+      
       <Gallery photos={photos} onClick={openLightbox} />
       <ModalGateway>
         {viewerIsOpen ? (
