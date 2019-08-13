d196844a8db48dc71850ce6667e13e91d65c2465
Jisan Zaman <jisan.zaman@dynamo.works>
2019-08-13 17:42:14 -0400

    update image refs

--- src/photos.js
+++ src/photos.js
@@ -1,158 +1,173 @@
+import * as theaNap from "./images/with_thea_nap.jpg";
+// import * as theaNap from "./images/with_thea_nap.jpg";
+// import * as theaNap from "./images/with_thea_nap.jpg";
+// import * as theaNap from "./images/with_thea_nap.jpg";
+// import * as theaNap from "./images/with_thea_nap.jpg";
+// import * as theaNap from "./images/with_thea_nap.jpg";
+// import * as theaNap from "./images/with_thea_nap.jpg";
+// import * as theaNap from "./images/with_thea_nap.jpg";
+// import * as theaNap from "./images/with_thea_nap.jpg"
+// import * as theaNap from "./images/with_thea_nap.jpg"
+// import * as theaNap from "./images/with_thea_nap.jpg"
+// import * as theaNap from "./images/with_thea_nap.jpg"
+// import * as theaNap from "./images/with_thea_nap.jpg"
+// import * as theaNap from "./images/with_thea_nap.jpg"
+
+
 export const photos = [
     {
-      src: `${process.env.PUBLIC_URL}/images/with_thea_nap.jpg`,
-      width: 3,
-      height: 4,
-      title: "Thank you for introducing me to your family. Especially this little cutie of a rascal. They are amazing and I am incredibly privileged to know them."
+        src: theaNap,
+        width: 3,
+        height: 4,
+        title: "Thank you for introducing me to your family. Especially this little cutie of a rascal. They are amazing and I am incredibly privileged to know them."
     },
     {
-      src: `${process.env.PUBLIC_URL}/images/vegan_fancy_dinner_2018.jpg`,
-      width: 3,
-      height: 4,
-      title: "Thank you for going vegan with me for a whole month. And keeping me honest. Elizabeth's Gone Raw was amazing. It was even more amazing to spend it there with you."
+        src: theaNap,
+        width: 3,
+        height: 4,
+        title: "Thank you for going vegan with me for a whole month. And keeping me honest. Elizabeth's Gone Raw was amazing. It was even more amazing to spend it there with you."
     },
     {
-      src: `${process.env.PUBLIC_URL}/images/happy_hour_bootcamp_2018.jpg`,
-      width: 8,
-      height: 4,
-      title: "Thank you for putting up with my second job. It takes a special person to deal with a partner with no weekends and no nights free. It was great to have you at this Happy Hour over at Crafthouse. You are an amazingly patient and kind partner"
+        src: theaNap,
+        width: 8,
+        height: 4,
+        title: "Thank you for putting up with my second job. It takes a special person to deal with a partner with no weekends and no nights free. It was great to have you at this Happy Hour over at Crafthouse. You are an amazingly patient and kind partner"
     },
     {
-      src: `${process.env.PUBLIC_URL}/images/housewarming_feb_2018.jpg`,
-      width: 5,
-      height: 4,
-      title: ""
+        src: theaNap,
+        width: 5,
+        height: 4,
+        title: ""
     },
     {
-      src: `${process.env.PUBLIC_URL}/images/axe_throwing.png`,
-      width: 4,
-      height: 4
+        src: theaNap,
+        width: 4,
+        height: 4
     },
     {
-      src: `${process.env.PUBLIC_URL}/images/fabulous_pose_dc.jpg`,
-      width: 3,
-      height: 4
+        src: theaNap,
+        width: 3,
+        height: 4
     },
     {
-      src: `${process.env.PUBLIC_URL}/images/bengali_wedding.jpg`,
-      width: 4,
-      height: 4
+        src: theaNap,
+        width: 4,
+        height: 4
     },
     {
-      src: `${process.env.PUBLIC_URL}/images/canvassing.jpg`,
-      width: 4,
-      height: 4
+        src: theaNap,
+        width: 4,
+        height: 4
     },
     {
-      src: `${process.env.PUBLIC_URL}/images/ceb_tower.jpg`,
-      width: 3,
-      height: 4
+        src: theaNap,
+        width: 3,
+        height: 4
     },
     {
-        src: `${process.env.PUBLIC_URL}/images/cherry_blossoms_2018.jpg`,
+        src: theaNap,
         width: 5,
         height: 3
     },
     {
-        src: `${process.env.PUBLIC_URL}/images/cwc_2019.jpg`,
+        src: theaNap,
         width: 5,
         height: 3
     },
     {
-        src: `${process.env.PUBLIC_URL}/images/cherry_blossoms_2018.jpg`,
+        src: theaNap,
         width: 5,
         height: 3
     },
     {
-        src: `${process.env.PUBLIC_URL}/images/cherry_blossoms_2018.jpg`,
+        src: theaNap,
         width: 5,
         height: 3
     },
     {
-        src: `${process.env.PUBLIC_URL}/images/cherry_blossoms_2018.jpg`,
+        src: theaNap,
         width: 5,
         height: 3
     },
     {
-        src: `${process.env.PUBLIC_URL}/images/cherry_blossoms_2018.jpg`,
+        src: theaNap,
         width: 5,
         height: 3
     },
     {
-        src: `${process.env.PUBLIC_URL}/images/cherry_blossoms_2018.jpg`,
+        src: theaNap,
         width: 5,
         height: 3
     },
     {
-        src: `${process.env.PUBLIC_URL}/images/cherry_blossoms_2018.jpg`,
+        src: theaNap,
         width: 5,
         height: 3
     },
     {
-        src: `${process.env.PUBLIC_URL}/images/cherry_blossoms_2018.jpg`,
+        src: theaNap,
         width: 5,
         height: 3
     },
     {
-        src: `${process.env.PUBLIC_URL}/images/cherry_blossoms_2018.jpg`,
+        src: theaNap,
         width: 5,
         height: 3
     },
     {
-        src: `${process.env.PUBLIC_URL}/images/cherry_blossoms_2018.jpg`,
+        src: theaNap,
         width: 5,
         height: 3
     },
     {
-        src: `${process.env.PUBLIC_URL}/images/cherry_blossoms_2018.jpg`,
+        src: theaNap,
         width: 5,
         height: 3
     },
     {
-        src: `${process.env.PUBLIC_URL}/images/cherry_blossoms_2018.jpg`,
+        src: theaNap,
         width: 5,
         height: 3
     },
     {
-        src: `${process.env.PUBLIC_URL}/images/cherry_blossoms_2018.jpg`,
+        src: theaNap,
         width: 5,
         height: 3
     },
     {
-        src: `${process.env.PUBLIC_URL}/images/cherry_blossoms_2018.jpg`,
+        src: theaNap,
         width: 5,
         height: 3
     },
     {
-        src: `${process.env.PUBLIC_URL}/images/cherry_blossoms_2018.jpg`,
+        src: theaNap,
         width: 5,
         height: 3
     },
     {
-        src: `${process.env.PUBLIC_URL}/images/cherry_blossoms_2018.jpg`,
+        src: theaNap,
         width: 5,
         height: 3
     },
     {
-        src: `${process.env.PUBLIC_URL}/images/cherry_blossoms_2018.jpg`,
+        src: theaNap,
         width: 5,
         height: 3
     },
     {
-        src: `${process.env.PUBLIC_URL}/images/cherry_blossoms_2018.jpg`,
+        src: theaNap,
         width: 5,
         height: 3
     },
     {
-        src: `${process.env.PUBLIC_URL}/images/cherry_blossoms_2018.jpg`,
+        src: theaNap,
         width: 5,
         height: 3
     },
     {
-        src: `${process.env.PUBLIC_URL}/images/cherry_blossoms_2018.jpg`,
+        src: theaNap,
         width: 5,
         height: 3
     }
 
-  ];
-  
+];
