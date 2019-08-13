c47a8aa483bbbf8228e2e9ee54aaf3d814ea9e84
Jisan Zaman <jisan.zaman@dynamo.works>
2019-08-13 16:34:44 -0400

    add photos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useCallback } from 'react';
+import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
+import { photos } from "./photos";
+
 import './App.css';
 
 function App() {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Happy Birthday Alissa Ortman</h1>
+      <p>I am so thankful you are in my life bae. I made you a website of 32 images and how thankful I am to share all those memories with you. Love,
+      Jisan</p>
+      <a 
+        href='https://photos.google.com/share/AF1QipNzEj98fcCRdo5XTrpe2rEZwX3Bz6FXpi8QCMGIsnOogZBGxZ1ARhYeaa5SH0q1Sg?key=TFNjS1pXQmFieXd4NDhwdTNPY3ZlT3oySVpaNV9R&source=ctrlq.org'>
+          {/* <img src='https://lh3.googleusercontent.com/_vmWevyFAySSaEnKhOIy04u6b1jPoBn3O2rEAamipu8sqddYcZPd186pjDCfYjPeQyxqWciT6TFNt33wjpbKDLk25k5_6KRG-mZJo0vr7_34jyZZ-kmBZNtRensg-CDd2ibGur3uQA=w2400' /> */}
+      </a>
+      <Gallery photos={photos} onClick={openLightbox} />
+      <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={photos.map(x => ({
+                ...x,
+                srcset: x.srcSet,
+                caption: x.title
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
     </div>
   );
 }
---
+++ src/photos.js
@@ -0,0 +1,50 @@
+export const photos = [
+    {
+      src: "https://lh3.googleusercontent.com/afEYt--EtFQlTe5vVzO66jVtP-IjNw886y_xhqnC87OizCc1_wmJeuasvBqexeH70WvL7rgYutm7NfAaacGV0gMnnZK1ONH4AfTrpsYCT4064vLCAAWnOUjBLcVG3zgUu6Wi15xpNVE43u4CaPNhJ_5ZArZS3VGoyDd0NfE1-ydbd0WqpHZifvkagQ3Inbudmx7QRELfsxlAlPgc5lglXY4aeIoN78pvRrQCzdCp4ckq-zpRsBjpeW6JKpQ1dINhS1t3crBOPfFNmLShw3Bp_Y6XiMpQs2Sf0HwvH4XRgU9NWWJkqwnR_rAANy8JLBMwlSu_qSCLL1c7D7qWy1zt4dWrDAmYrE5EUpWIVFJPiLfNZnhbTdzA9jb6KB2C5Si62cXGu515HFw5WGzavic1GK2BlNYE85OO5XotkPjBbc6iGFgNn3YVH-qfIz4HsXrQF9GB3Lbe03MtcMCvWwbX8JrIlhC82w0nDfnXDjiXUcpSBiD_-_HnyyH8rYGYHvkm8sT6D_VsrA1_gFSi3g5I_H3ZwSf4Kleev3StOXputEwYcQKg0bZctfX6Dle7Sbd3y286UwOSRvpAi9AMCcyRVazhFymPH9it7svEnjrOq3pAeNeKwZ1En24Gk31yla8w88LyoPPm41lFsUJQsaUUpUMEoQ=w1398-h1862-no",
+      width: 3,
+      height: 4,
+      title: "Thank you for introducing me to your family. Especially this little cutie of a rascal. They are amazing and I am incredibly privileged to know them."
+    },
+    {
+      src: "https://lh3.googleusercontent.com/Vnb6vTeizmUTbTujJtutBjz6nK4IY4TjXH2FchfoYeLenzKfjHnlBw2OzmNX-iDDdeRrnGIzAloTwNSIL9tu1OM0_4azAnEOGKXOi_fUgNpH6ZkjIw9EbV952YBMCfUKMjCmfvJHjmARFX6FlqBgRB_UdvA3laPS6EF-GDzEaH_JdmQSJ1CTew9i2Szu0MrVK2QnxOchvlRcOI8b1IHF-ETzOuRfn6543Bvn-zkovu3OHGz1LX0rQsU3WlvHsYuQ7pT4HOAE_vRveHD2fMCJyrURfEfuoXITZEI6OHzFLReKNc_t9ncmoW3wu6-VR62C7GdM3MedX1hVGSgtLfxzUucxpZ9arFpoZKMQPWgE4ycvZleoaCL95G7Uv4mRfp1E79KBzevwkZmU3tlxismi_uXahWF7Jbjnv-xtSwgQcsIrNY0iIxJGAb4_9IHygn_4ye9GHwxicUJHsP3QCb1YdaWS7p-QLJZdOtW0U7CdUisD7fo4pE_YmxAXc2PtYdVqePp5YT10McsqHoz2qVQRCSU6R1VBXXbzsqdMXbR5K6Y0Ne1vjIYEK3PtUvglW_tReD97SqyNVjdSj2HWgQTziRSgX86MwGBc4b1th96xG77Uvxm4OgJjrjmkk0lpBuarCZZpI5hN894dUFdI5zdYQvUVqg=w1398-h1862-no",
+      width: 3,
+      height: 4,
+      title: "Thank you for going vegan with me for a whole month. And keeping me honest. Elizabeth's Gone Raw was amazing. It was even more amazing to spend it there with you."
+    },
+    {
+      src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
+      width: 3,
+      height: 4
+    },
+    {
+      src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
+      width: 3,
+      height: 4
+    },
+    {
+      src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
+      width: 3,
+      height: 4
+    },
+    {
+      src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
+      width: 4,
+      height: 3
+    },
+    {
+      src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
+      width: 3,
+      height: 4
+    },
+    {
+      src: "https://source.unsplash.com/PpOHJezOalU/800x599",
+      width: 4,
+      height: 3
+    },
+    {
+      src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
+      width: 4,
+      height: 3
+    }
+  ];
+  
