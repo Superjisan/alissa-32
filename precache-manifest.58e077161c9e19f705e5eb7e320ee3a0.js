9010409c0af6cb47d18b21854f71dbcf7987e519
Jisan Zaman <jisan.zaman@dynamo.works>
2019-08-13 15:45:09 -0400

    Updates

---
+++ precache-manifest.58e077161c9e19f705e5eb7e320ee3a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65710f33a3bb3e6618b6f679b049d1a1",
+    "url": "/alissa-32/index.html"
+  },
+  {
+    "revision": "777a20d32cd0824afce0",
+    "url": "/alissa-32/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "68267c8733ed6ab76911",
+    "url": "/alissa-32/static/js/2.35ec7ccc.chunk.js"
+  },
+  {
+    "revision": "777a20d32cd0824afce0",
+    "url": "/alissa-32/static/js/main.254429e2.chunk.js"
+  },
+  {
+    "revision": "454c8c89cc0d362190d9",
+    "url": "/alissa-32/static/js/runtime~main.34d8e2bf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/alissa-32/static/media/logo.5d5d9eef.svg"
+  }
+]);
